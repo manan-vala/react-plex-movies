e2bb325f2cfeb6896931171a8729429738948acf
Manan Vala <mananvala15@gmail.com>
2025-07-10 00:01:11 +0530

    final deployment change BrowserRoute to HashRoute

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Section from "./Section";
 import MovieDetails from "./MovieDetails";
@@ -8,13 +8,13 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Section />} />
           <Route path="/movies/:id" element={<MovieDetails />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
